766bcb23540e5470500bf795474b6a692375aa32
emyjamalian <emy.jamalian@gmail.com>
2023-11-14 11:18:34 +0100

    put js at the end

--- js/form.js
+++ js/form.js
@@ -3,16 +3,18 @@ let card = document.querySelector(".question-card");
 const main = document.querySelector('[data-js="main"]');
 const formButton = document.querySelector(".form__button");
 
-console.log(question);
-
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const formData = new FormData(event.target);
   const newCard = Object.fromEntries(formData);
 
-  console.log(newCard);
-  const newItem = `
+  const newQuestion = newCard.question.innerHtml;
+  const newAnswer = newCard.answer.innerHtml;
+  const newTag = newCard.tag.innerHtml;
+
+  const newItem = () => {
+    `
     <sectionclass="question-card">
     <img
       class="question-card__icon"
@@ -20,17 +22,14 @@ form.addEventListener("submit", (event) => {
       src="./assets/bookmark.png"
       alt="bookmark"
     />
-    <h2 class="question-card__title">${newCard.question}</h2>
+    <h2 class="question-card__title">${newQuestion}</h2>
     <button class="question-card__button" data-js="question-button">
     show answer
   </button>
-  <p class="question-card__answer"> ${newCard.answer} </p>
+  <p class="question-card__answer"> ${newAnswer} </p>
   <div class="question-card__tags">
-  <div class="question-card__tag">${newCard.tag}</div>
+  <div class="question-card__tag">${newTag}</div>
   </section>`;
-
+  };
   console.log(newItem);
-
-  // Update the content of the card element directly
-  card = newItem.innerhtml;
 });
--- js/index.js
+++ js/index.js
@@ -1,11 +1,9 @@
-let showanswerButton = document.querySelector('data-js="question-button"');
+const showanswerButton = document.querySelector('[data-js="question-button"]');
 const answer = document.querySelector(".question-card__answer");
-const bookmarkIcon = document.querySelector('data-js="question-card-icon"');
-
-showanswerButton.addEventListener("click", showAnswer);
-bookmarkIcon.addEventListener("click", bookmarked);
+const bookmarkIcon = document.querySelector('[data-js="question-card-icon"]');
 
 function showAnswer() {
+  console.log("showAnswer function called");
   if (answer.style.display === "none") {
     showanswerButton.classList.add("hide-answer");
     answer.style.display = "block";
@@ -18,5 +16,9 @@ function showAnswer() {
 }
 
 function bookmarked() {
+  console.log("bookmarked function called");
   bookmarkIcon.classList.toggle("question-card__icon--active");
 }
+
+showanswerButton.addEventListener("click", showAnswer);
+bookmarkIcon.addEventListener("click", bookmarked);
